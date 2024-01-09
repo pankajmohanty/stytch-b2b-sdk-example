d672a77fb04fbac03d0320fc1bca9aec6519d1a4
pankajmohanty <51083695+pankajmohanty@users.noreply.github.com>
2024-01-09 19:19:00 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,16 +17,6 @@ const stytch = createStytchB2BUIClient(
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Stytch Next.js B2B Example</title>
-        <meta
-          name="description"
-          content="An example Next.js B2B application using Stytch for authentication"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-
       <StytchB2BProvider stytch={stytch}>
         <main>
           <div className="container">
