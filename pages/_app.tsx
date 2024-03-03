d6dc4deecaffa9463b2bd81842276e62518c518b
pankajmohanty <51083695+pankajmohanty@users.noreply.github.com>
2024-03-03 19:09:43 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,33 @@
 import "../styles/stytch.css";
 import type { AppProps } from "next/app";
-import { StytchB2BProvider, useStytchB2BClient } from "@stytch/nextjs/b2b";
+import { StytchB2BProvider } from "@stytch/nextjs/b2b";
+import { createStytchB2BUIClient } from '@stytch/nextjs/b2b/ui';
 import React from "react";
 import Head from "next/head";
 
-const stytch = useStytchB2BClient();
-StytchB2BProvider.init({
-  publicToken: process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN ?? "",
-  cookieOptions: {
-    jwtCookieName: `stytch_session_jwt_next_b2b_app`,
-  },
-});
+const stytch = createStytchB2BUIClient(
+  process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN ?? "",
+  {
+    cookieOptions: {
+      jwtCookieName: `stytch_session_jwt_next_b2b_app`,
+      opaqueTokenCookieName: `stytch_session_next_b2b_app`,
+    },
+  }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Stytch Next.js B2B Example</title>
         <meta
           name="description"
           content="An example Next.js B2B application using Stytch for authentication"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
+
       <StytchB2BProvider stytch={stytch}>
         <main>
           <div className="container">
